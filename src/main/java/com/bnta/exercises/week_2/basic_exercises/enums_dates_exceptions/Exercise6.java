42188451ab6679d179758056bbcf42d56edfee6c
Rachel Kaur <rachel.km@outlook.com>
2022-02-06 12:11:22 +0000

    Added more comments to Exercise 6 (Enums, Dates, & Exceptions)

--- src/main/java/com/bnta/exercises/week_2/basic_exercises/enums_dates_exceptions/Exercise6.java
+++ src/main/java/com/bnta/exercises/week_2/basic_exercises/enums_dates_exceptions/Exercise6.java
@@ -30,20 +30,27 @@ public class Exercise6 {
         for (int i = 0; i < stringArr.length; i++) {
             // Test for errors while code block is executed
             try {
+                // Parse strArr, returning integers and adding them to the string convertedToInt, each separated by a comma and space
                 // convertedToInt += Integer.parseInt(stringArr[i]) + " ";
                 convertedToInt += Integer.parseInt(stringArr[i]) + ", ";
+                //
+                // Parse strArr, returning integers and adding them to the total sum integer variable
                 sum += Integer.parseInt(stringArr[i]);
             // Execute code block if error occurs in the try block
             } catch (Exception e) {
+                // Add strArray elements that cannot be converted to integers, to the string notConvertedToInt, each separated by a comma and space
                 // notConvertedToInt += stringArr[i] + " ";
                 notConvertedToInt += stringArr[i] + ", ";
             }
         }
 
+        // Print sum of integers together with converted integers, each separated by a comma and space (except for the last integer in the list, i.e. length - 2 removes that comma and space)
         // System.out.println("Sum of integers [" + convertedToInt + "]: " + sum);
         System.out.println("Sum of integers [" + convertedToInt.substring(0, convertedToInt.length() - 2) + "]: " + sum);
+
+        // Print those elements that were not converted to integers, using the same pattern as above
         // System.out.println("Not able to convert to integers: " + notConvertedToInt);
-        System.out.println("Not able to convert to integers: " + notConvertedToInt.substring(0, notConvertedToInt.length() - 2));
+        System.out.println("Not able to convert to integers: [" + notConvertedToInt.substring(0, notConvertedToInt.length() - 2) + "]");
 
     }
 
