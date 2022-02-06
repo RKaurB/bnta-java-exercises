c04563f36b802d677fa8293addbd22d9c93469f2
Rachel Kaur <rachel.km@outlook.com>
2022-02-06 02:28:13 +0000

    Commit

--- src/main/java/com/bnta/exercises/week_2/basic_exercises/enums_dates_exceptions/Exercise6.java
+++ src/main/java/com/bnta/exercises/week_2/basic_exercises/enums_dates_exceptions/Exercise6.java
@@ -10,8 +10,35 @@ Q6:  Try catch
 
 package com.bnta.exercises.week_2.basic_exercises.enums_dates_exceptions;
 
+import java.util.Arrays;
+
 public class Exercise6 {
 
+    public static void main(String[] args) {
 
+        String[] stringArr = {"1", "2", "a", "b", "foo", "3"};
+
+        int sum = 0;
+
+        String notConvertedToInt = "";
+        String convertedToInt = "";
+
+        for (int i = 0; i < stringArr.length; i++) {
+            try {
+                // convertedToInt += Integer.parseInt(stringArr[i]) + " ";
+                convertedToInt += Integer.parseInt(stringArr[i]) + ", ";
+                sum += Integer.parseInt(stringArr[i]);
+            } catch (Exception e) {
+                // notConvertedToInt += stringArr[i] + " ";
+                notConvertedToInt += stringArr[i] + ", ";
+            }
+        }
+
+        // System.out.println("Sum of integers [" + convertedToInt + "]: " + sum);
+        System.out.println("Sum of integers [" + convertedToInt.substring(0, convertedToInt.length() - 2) + "]: " + sum);
+        // System.out.println("Not able to convert to integers: " + notConvertedToInt);
+        System.out.println("Not able to convert to integers: " + notConvertedToInt.substring(0, notConvertedToInt.length() - 2));
+
+    }
 
 }
