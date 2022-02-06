982794c50bee902243350d3005b4f56902eeb96e
Rachel Kaur <rachel.km@outlook.com>
2022-02-06 13:53:36 +0000

    Added comments and minor changes to Exercise 2 (Arrays, Conditionals, & Methods)

--- src/main/java/com/bnta/exercises/week_2/arrays_conditionals_methods/Exercise2.java
+++ src/main/java/com/bnta/exercises/week_2/arrays_conditionals_methods/Exercise2.java
@@ -8,6 +8,14 @@ package com.bnta.exercises.week_2.arrays_conditionals_methods;
 
 public class Exercise2 {
 
+    // Main method
+    public static void main(String[] args) {
+        // Original string input
+        String input = "   b ri   ght net   work  ";
+        // Prints out the string with all spaces removed
+        System.out.println("Re-formatted string: " + removeSpaces(input));
+    }
+
     // removeSpaces method - removes spaces and capitalises selected letters in the pre-defined string input, in accordance with desired output
     public static String removeSpaces(String input) {
         // Prints out original string input, for comparison with re-formatted string output
@@ -18,14 +26,6 @@ public class Exercise2 {
         System.out.println("String without spaces: " + withoutSpaces);
         // Returns characters 0 to 13 of the string, with a capitalised first character (B), a space after the 6th character (between t and n), and a capitalised 7th character (N)
         return withoutSpaces.substring(0,1).toUpperCase() + withoutSpaces.substring(1,6) + " " + withoutSpaces.substring(6,7).toUpperCase() + withoutSpaces.substring(7,13);
-    }
-
-    // Main method
-    public static void main(String[] args) {
-        // Original string input
-        String input = "   b ri   ght net   work  ";
-        // Prints out the string with all spaces removed
-        System.out.println("Re-formatted string: " + removeSpaces(input));
     }
 
 }
