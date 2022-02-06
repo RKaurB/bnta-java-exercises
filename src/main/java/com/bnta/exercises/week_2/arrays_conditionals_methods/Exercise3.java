7521e3706f2440d60eb019cb974f672a6e0e250b
Rachel Kaur <rachel.km@outlook.com>
2022-02-06 14:01:42 +0000

    Added comments and minor changes to Exercise 3 (Arrays, Conditionals, & Methods)

--- src/main/java/com/bnta/exercises/week_2/arrays_conditionals_methods/Exercise3.java
+++ src/main/java/com/bnta/exercises/week_2/arrays_conditionals_methods/Exercise3.java
@@ -9,22 +9,33 @@ import java.util.Scanner;
 
 public class Exercise3 {
 
+    // Main method
     public static void main(String[] args) {
+        // Create scanner variable to store user input
         Scanner input = new Scanner(System.in);
-        int num;
+        // Initialise num variable
+        // int num;
+        // Request user input
         System.out.println("Enter a number: ");
-        num = input.nextInt();
-        System.out.println(oddOrEven(num));
+        // Read user input from scanner, and store in num variable
+        int num = input.nextInt();
+        // Call oddOfEven method and print out the result
+        System.out.println("This number [" + num + "] is " + oddOrEven(num));
     }
 
+    // oddOrEven method
     public static String oddOrEven(int num) {
         // String result = "";
+        // Create result variable
         String result;
+        // If the entered number is divisible by 2 with no remainder, it is an even number
         if (num % 2 == 0) {
-            result = "Even";
+            result = "even";
         } else {
-            result = "Odd";
+            // Else, the number is odd
+            result = "odd";
         }
+        // Return the result (i.e. even/odd)
         return result;
     }
 }
