3131defe84917b18d568ca6d91af348fb48b05ce
Rachel Kaur <rachel.km@outlook.com>
2022-02-06 13:51:12 +0000

    Added comments and minor changes to Exercise 2 (Arrays, Conditionals, & Methods)

--- src/main/java/com/bnta/exercises/week_2/arrays_conditionals_methods/Exercise2.java
+++ src/main/java/com/bnta/exercises/week_2/arrays_conditionals_methods/Exercise2.java
@@ -8,20 +8,24 @@ package com.bnta.exercises.week_2.arrays_conditionals_methods;
 
 public class Exercise2 {
 
+    // removeSpaces method - removes spaces and capitalises selected letters in the pre-defined string input, in accordance with desired output
     public static String removeSpaces(String input) {
+        // Prints out original string input, for comparison with re-formatted string output
+        System.out.println("Original string: " + input);
         // replaceAll - deletes all whitespace in the string, using replacement
         String withoutSpaces = input.replaceAll(" ", "");  // can also use input.replaceAll("\s", "")
-        // prints out the string with all spaces removed
-        System.out.println(withoutSpaces);
-        // returns characters 0 to 13 of the string, with a capitalised first character (B), a space after the 6th character (between t and n), and a capitalised 7th character (N)
+        // Prints out string with all spaces removed
+        System.out.println("String without spaces: " + withoutSpaces);
+        // Returns characters 0 to 13 of the string, with a capitalised first character (B), a space after the 6th character (between t and n), and a capitalised 7th character (N)
         return withoutSpaces.substring(0,1).toUpperCase() + withoutSpaces.substring(1,6) + " " + withoutSpaces.substring(6,7).toUpperCase() + withoutSpaces.substring(7,13);
     }
 
+    // Main method
     public static void main(String[] args) {
-        // original string input
+        // Original string input
         String input = "   b ri   ght net   work  ";
-        // prints out original string input, for comparison with output
-        System.out.println(removeSpaces(input));
+        // Prints out the string with all spaces removed
+        System.out.println("Re-formatted string: " + removeSpaces(input));
     }
 
 }
