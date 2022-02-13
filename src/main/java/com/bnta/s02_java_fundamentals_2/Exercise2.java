2911b386db98b8e48ffe758bfe85a54a8ec22d58
Rachel Kaur <rachel.km@outlook.com>
2022-02-13 16:43:53 +0000

    Amend Exercise 2 (Java Fundamentals)

--- src/main/java/com/bnta/s02_java_fundamentals_2/Exercise2.java
+++ src/main/java/com/bnta/s02_java_fundamentals_2/Exercise2.java
@@ -6,13 +6,34 @@
 
 package com.bnta.s02_java_fundamentals_2;
 
+import java.util.Scanner;
+
 public class Exercise2 {
+
     public static void main(String[] args) {
-        String str1 = "Hello";
-        // testing
-        // String str2 = "Hello";
-        String str2 = "hello";
-        boolean compareStrings = str1.equals(str2);
-        System.out.println(compareStrings);
+//        String str1 = "Hello";
+//        // testing
+//        // String str2 = "Hello";
+//        String str2 = "hello";
+//        boolean compareStrings = str1.equals(str2);
+//        System.out.println(compareStrings);
+//    }
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a word: ");
+        String inputString1 = scanner.nextLine();
+        System.out.println("Enter another word: ");
+        String inputString2 = scanner.nextLine();
+        boolean compareStrings = inputString1.equals(inputString2);
+
+        if (compareStrings) {
+            System.out.println("Comparing strings: " + inputString1 + ", " + inputString2 + "...");
+            System.out.println("Strings are equal!");
+            ;
+        } else {
+            System.out.println("Comparing strings: " + inputString1 + ", " + inputString2 + "...");
+            System.out.println("Strings are not equal!");
+        }
+
     }
 }
