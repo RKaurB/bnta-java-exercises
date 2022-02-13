2bee1792ed14ffcf5cd070b313f287d57998f263
Rachel Kaur <rachel.km@outlook.com>
2022-02-13 17:43:33 +0000

    Completed Exercise 4 (Arrays, Conditionals, & Methods)

--- src/main/java/com/bnta/s06_arrays_conditionals_methods/Exercise4.java
+++ src/main/java/com/bnta/s06_arrays_conditionals_methods/Exercise4.java
@@ -5,24 +5,47 @@
 
 package com.bnta.s06_arrays_conditionals_methods;
 
+import java.util.Scanner;
+
 public class Exercise4 {
 
     // MAIN FUNCTION
     public static void main(String[] args) {
+        System.out.println("Enter a number: ");
         // Use Scanner to get and read user input
+        Scanner input = new Scanner(System.in);
         // Store that input in a variable (int)
-        // Call the isPrimeNums method
+        int num = input.nextInt();
+        // Call isPrimeNums method, and print out the result
+        // System.out.println("This number [" + num + "] is " + oddOrEven(num));
+        if (isPrimeNums(num)) {
+            System.out.println(num + " is a prime number!");
+        } else {
+            System.out.println(num + " is not a prime number!");
+        }
     }
 
-    // PRIME NUMBERS METHOD
-    // What is a prime number?
-    // a number that is divisible only by itself and 1 (e.g. 2, 3, 5, 7, 11)
-    public static void isPrimeNums(int num) {
-        // if input is divisible only by itself and 1
-            // then set boolean to true
-        // else
-            // set boolean to false
-    }
+        // PRIME NUMBERS METHOD
+        // What is a prime number?
+        // a number that is divisible only by itself and 1 (e.g. 2, 3, 5, 7, 11)
+        public static boolean isPrimeNums(int num){
 
-}
+            // If number is less than or equal to 1, return false
+            if (num <= 1) {
+                return false;
+            }
+
+            // Check from 2 to number-1
+            for (int i = 2; i < num; i++) {
+                // If remainder of number divided by i (num % i) is equal to 0, return false
+                if (num % i == 0) {
+                    return false;
+                }
+            }
+
+            return true;
+
+        }
+
+    }
 
